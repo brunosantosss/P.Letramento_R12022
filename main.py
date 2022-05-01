1f833c148fde37283c2e90a2a27f75727ae5d5d9
BrunoOh <85379055+BrunoOh79@users.noreply.github.com>
2022-05-01 16:24:16 -0300

    Arquivos essenciais

---
+++ main.py
@@ -0,0 +1,20 @@
+from pytesseract import pytesseract
+import pyttsx3
+import cv2
+
+# Lendo a imagem
+img = cv2.imread("liquidificador.jpeg")
+
+# Iniciando o motor de reproducao da voz
+voice = pyttsx3.init()
+
+# Caminho de instalacao do executavel do tesseract 
+caminho_tesseract = r"C:\Users\Bruno\AppData\Local\Programs\Tesseract-OCR\tesseract.exe"
+pytesseract.tesseract_cmd = caminho_tesseract
+
+# Transformando a imagem para string
+img_texto = pytesseract.image_to_string(img, lang='por')
+
+print(img_texto)
+voice.say(img_texto)
+voice.runAndWait()
