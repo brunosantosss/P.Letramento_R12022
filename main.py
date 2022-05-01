b07d174c6ef36798a8632ecd27244f6519b11091
BrunoOh <85379055+BrunoOh79@users.noreply.github.com>
2022-05-01 17:06:51 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import pyttsx3
 import cv2
 
 # Lendo a imagem
-img = cv2.imread("elo.jpeg")
+img = cv2.imread("elo.jpeg") # Altere para o nome da imagem que deseja ler em voz alta
 
 # Iniciando o motor de reproducao da voz
 voice = pyttsx3.init()
 
 # Caminho de instalacao do executavel do tesseract 
-caminho_tesseract = r"C:\Users\Bruno\AppData\Local\Programs\Tesseract-OCR\tesseract.exe"
+caminho_tesseract = r"C:\Users\USER\AppData\Local\Programs\Tesseract-OCR\tesseract.exe" # Altere o caminho para o seu tesseract.exe
 pytesseract.tesseract_cmd = caminho_tesseract
 
 # Transformando a imagem para string
